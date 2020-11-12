fa5e7717d5af6a9914641b5abba060372d8f7143
Akshatha.Appajigowda <akshathaa0108@gmail.com>
2020-11-12 12:00:46 +0100

    first commit completed home page

---
+++ src/Home.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import"./Home.css";
+import Product from './Product';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img
+                    className="home__image"
+                    src="https://images-eu.ssl-images-amazon.com/images/G/03/kindle/journeys/YTNkYTJjMWMt/YTNkYTJjMWMt-MTBkYjI1ZjMt-w1500._CB417540758_.jpg"
+                    alt=""
+                />
+
+                <div className="home__row">
+                    <Product
+                        title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
+                        price={29.67}
+                        image="https://images-na.ssl-images-amazon.com/images/I/510AO7UjVPL._SX331_BO1,204,203,200_.jpg"
+                        rating={5}
+                    />
+                    <Product 
+                        title="Zero to One: Notes on Start Ups, or How to Build the Futures"
+                        price={38.17}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51qUTPaUfCL._SX317_BO1,204,203,200_.jpg"
+                        rating={4}
+                    />
+                </div>
+
+                <div className="home__row">
+                    <Product 
+                        title="Customer Journey 4.0:How to create the Customer Journey Map "
+                        price={61.71}
+                        image="https://images-na.ssl-images-amazon.com/images/I/41jErHVfWIL._SX322_BO1,204,203,200_.jpg"
+                        rating={3}
+                    />
+                    <Product 
+                        title="Intelligent Investor: The Classic Text on Value Investing (Rough Cut) "
+                        price={21.66}
+                        image="https://images-na.ssl-images-amazon.com/images/I/410p7Yf1iYL._SX332_BO1,204,203,200_.jpg"
+                        rating={4}
+                    />
+                    <Product 
+                        title="Brave New World: Textbook (Dieste Travel Neuspr Ach Library English Department "
+                        price={10.91}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51JumKTNPvL._SX323_BO1,204,203,200_.jpg"
+                        rating={4}
+                    />
+                </div>
+
+                <div className="home__row">
+                    <Product 
+                        title="The Hogwarts Library Boxed Set including Fantastic Beasts & Where to Find Themp "
+                        price={71.95}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51IefD0JDUL._SX376_BO1,204,203,200_.jpg"
+                        rating={5}
+                    />
+                </div> 
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA1JsU4PzejI7snnopoK0sPMia6HLxAh_Q",
+    authDomain: "clone-63238.firebaseapp.com",
+    databaseURL: "https://clone-63238.firebaseio.com",
+    projectId: "clone-63238",
+    storageBucket: "clone-63238.appspot.com",
+    messagingSenderId: "6335554426",
+    appId: "1:6335554426:web:7be9a87fb170f01c57ecc3",
+    measurementId: "G-2909YJC0S2"
+  };
